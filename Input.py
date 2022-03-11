173709eb43ca7b456fefa64311ab0bed17f8bbf9
HunterOBrien <obrienh2@middleton.school.nz>
2022-03-11 18:04:29 +1300

    Update

---
+++ Input.py
@@ -0,0 +1,7 @@
+""" program to demonstrate input
+"""
+name = input("Name: ")
+num_1 = int(input("num_1: "))
+num_2 = int(input("num_2: "))
+
+print(f"Hello {name}, {num_1} + {num_2} = {num_1 + num_2}")
